328e03503635f16a2d71bd0ac7e60f1b275515fe
Bao Nhat <baonhat20@gmail.com>
2023-05-25 07:35:10 +0700

    123

--- projects/admin-hotel/src/app/components/chart/pie-chart/pie-chart.component.ts
+++ projects/admin-hotel/src/app/components/chart/pie-chart/pie-chart.component.ts
@@ -1,74 +1,89 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart } from 'angular-highcharts';
+import { ApiService } from '../../../_service/api.service';
 @Component({
-  selector: 'app-pie-chart',
-  templateUrl: './pie-chart.component.html',
-  styleUrls: ['./pie-chart.component.css']
+    selector: 'app-pie-chart',
+    templateUrl: './pie-chart.component.html',
+    styleUrls: ['./pie-chart.component.css']
 })
 export class PieChartComponent implements OnInit {
+    chart!: any
+    currentYear!: number;
+    revenueForRoom!: any[];
+    priceRoom!: number;
+    percentRoomType!: number;
+    totalPrice!: number;
+    constructor(private api: ApiService) { }
 
-    chart = new Chart({
-        chart: {
-          type: 'pie',
-          height: 325
-        },
-        // title: {
-        // //   text: 'Category wise sales'
-        // },
-        xAxis: {
-          categories: [
-            'Electronics',
-            'Groceries',
-            'Cosmetics',
-            'Clothes',
-            'Appliances',
-          ]
-        },
-        yAxis: {
-          title: {
-            text: 'Revenue in %'
-          }
-        },
-        series: [
-         {
-          type: 'pie',
-          data: [
-            {
-              name: 'Electronics',
-              y: 41.0,
-              color: '#044342',
-            },
-            {
-              name: 'Groceries',
-              y: 33.8,
-              color: '#7e0505',
-            },
-            {
-              name: 'Cosmetics',
-              y: 6.5,
-              color: '#ed9e20',
-            },
-            {
-              name: 'Clothes',
-              y: 15.2,
-              color: '#6920fb',
-            },
-            {
-              name: 'Appliances',
-              y: 3.5,
-              color: '#121212',
-            },
-          ]
-         }
-        ],
-        credits: {
-          enabled: false
-        }
-      })
-    
-      constructor() { }
-    
-      ngOnInit(): void {
-      }
-    
-  }
+    ngOnInit(): void {
+        this.currentYear = new Date().getFullYear();
+        this.api.getRevenueForRoom(this.currentYear).subscribe((res: any) => {
+
+            this.revenueForRoom = res
+            let totalPrice = 0;
+            // for (let i = 0; i < res[i].price; i++) {
+            //     let percentage = (res[i].price / totalPrice) * 100;
+                
+            //    console.log(percentage);
+               
+                
+            // }
+
+            for (let i = 0; i < res.length; i++) {
+                totalPrice += res[i].price;
+                this.totalPrice = totalPrice
+            }
+            this.chart = new Chart({
+                chart: {
+                    type: 'pie',
+                    height: 350,
+                    width: 350
+                },
+                title: {
+                  text: 'The best rooms type in year'
+                },
+                xAxis: {
+                    categories: this.revenueForRoom.map(item => item.typeRoomName)
+                },
+                yAxis: {
+                    title: {
+                        text: 'Revenue in %'
+                    }
+                },
+                
+                series: [
+                    {
+                        type: 'pie',
+                        data: [
+                            {
+                                name: res[0].typeRoomName,
+                                y: Math.ceil((res[0].price / this.totalPrice)*100 ) ,
+                                color: '#4A5959',
+                               
+                            },
+                            {
+                                name: res[1].typeRoomName,
+                                y: Math.ceil((res[1].price / this.totalPrice)*100),
+                                color: '#F0B82C',
+                            },
+                            {
+                                name: res[2].typeRoomName,
+                                y: Math.ceil((res[2].price / this.totalPrice)*100),
+                                color: '#B7C9C9',
+                            },
+
+                        ]
+                    }
+                ],
+                credits: {
+                    enabled: false
+                }
+            })
+
+        })
+    }
+
+
+
+
+}
