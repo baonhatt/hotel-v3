cfbb89820595365f28f0ff74eba0d395bfdcc52d
metaron23 <hungmetaron2@gmail.com>
2023-05-26 07:13:17 +0700

    revenue

--- projects/admin-hotel/src/app/environments/environment.development.ts
+++ projects/admin-hotel/src/app/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-//   BASE_URL_API : "https://webhotel.click",
-  BASE_URL_API : "https://localhost:7103",
-  BASE_URL_WEB : "http://localhost:8000",
+  BASE_URL_API : "https://webhotel.click",
+//   BASE_URL_API : "https://localhost:7103",
+  BASE_URL_WEB : "https://user.webhotel.click",
 };
