7c19ba713d6a7332c29b71c9ffb91929bf44ee3f
metaron23 <hungmetaron2@gmail.com>
2023-05-30 11:30:38 +0700

    revenue

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   BASE_URL_API: 'https://localhost:7103',
   BASE_URL_WEB: 'http://localhost:4200',
-  QR_MOMO: `https://webhotel.click/user/payment/momoQR`,
+  QR_MOMO: `https://localhost:7103/user/payment/momoQR`,
 
   spaceId: 'e76861lm8t6b',
   accessToken: 'pJnp8KyAqAWRsXB0-m0p0ULvJnQRYHxB-QIRkK1AUPw',
